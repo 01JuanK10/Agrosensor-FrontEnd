0a8ccabf88483a5c975ff77aa09c853dc9e14451
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-24 20:49:26 -0500

    Add: New Topbar component

---
+++ src/app/features/home/public-topbar/public-topbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-public-topbar',
+  imports: [RouterLink],
+  templateUrl: './public-topbar.html',
+  styleUrl: './public-topbar.scss',
+})
+export class PublicTopbar {
+
+}
