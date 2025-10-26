84297c9407b4a5db7b80164c03297437d87dcbec
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-25 20:49:47 -0500

    feat: Home page first version

--- src/app/features/home/public-home/public-home.ts
+++ src/app/features/home/public-home/public-home.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { PublicTopbar } from '../public-topbar/public-topbar';
 import { HomeSlider } from '../home-slider/home-slider';
+import { Footer } from '../../core/footer/footer';
 
 @Component({
   selector: 'app-public-home',
-  imports: [PublicTopbar, HomeSlider],
+  imports: [PublicTopbar, HomeSlider, Footer],
   templateUrl: './public-home.html',
   styleUrl: './public-home.scss',
 })
